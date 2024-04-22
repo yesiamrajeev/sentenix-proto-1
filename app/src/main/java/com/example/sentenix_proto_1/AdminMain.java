7aea85c3a57f117ded9ee2bbb36f05d0c57f28ca
rajee <kumarrajeevbehera2019@gmail.com>
2024-04-23 02:22:21 +0530

    deleted admin java and xml files for home notification

--- app/src/main/java/com/example/sentenix_proto_1/AdminMain.java
+++ app/src/main/java/com/example/sentenix_proto_1/AdminMain.java
@@ -9,10 +9,8 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.TextView;
 
-import com.example.sentenix_proto_1.Fragments.HomeAdminFragment;
 import com.example.sentenix_proto_1.Fragments.HomeFragment;
 import com.example.sentenix_proto_1.Fragments.ProfileFragment;
-import com.example.sentenix_proto_1.Fragments.AdminNotificationFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -33,20 +31,20 @@ public class AdminMain extends AppCompatActivity {
         // Initialize the first fragment when activity is created
         loadFragment(new HomeFragment()); // Use constructor to create HomeFragment instance
 
-        bottomNavigationView.setOnItemSelectedListener(new BottomNavigationView.OnItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                int id = item.getItemId();
-                if (id == R.id.nav_home) {
-                    loadFragment(new HomeAdminFragment());
-                } else if (id == R.id.nav_notifications) {
-                    loadFragment(new AdminNotificationFragment());
-                } else if (id == R.id.nav_profile) {
-                    loadFragment(new ProfileFragment());
-                }
-                return true; // Return true to indicate the item is selected
-            }
-        });
+//        bottomNavigationView.setOnItemSelectedListener(new BottomNavigationView.OnItemSelectedListener() {
+//            @Override
+//            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+//                int id = item.getItemId();
+//                if (id == R.id.nav_home) {
+//                    loadFragment(new HomeAdminFragment());
+//                } else if (id == R.id.nav_notifications) {
+//                    loadFragment(new AdminNotificationFragment());
+//                } else if (id == R.id.nav_profile) {
+//                    loadFragment(new ProfileFragment());
+//                }
+//                return true; // Return true to indicate the item is selected
+//            }
+//        });
 
         // Fetch and display the username
         FirebaseUser currentUser = mAuth.getCurrentUser();
