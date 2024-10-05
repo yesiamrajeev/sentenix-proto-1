e9f5e9fc276f8c1b38171af8a19a64d2526ff0d7
Yajy <goswamiyajy@gmail.com>
2024-10-05 12:38:01 +0530

    updated version of the app

--- app/src/main/java/com/example/sentenix_proto_1/AdminMain.java
+++ app/src/main/java/com/example/sentenix_proto_1/AdminMain.java
@@ -24,6 +24,7 @@ public class AdminMain extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Just testing
         setContentView(R.layout.admin_main);
 
         mAuth = FirebaseAuth.getInstance();
